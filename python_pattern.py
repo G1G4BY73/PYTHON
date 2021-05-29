cd13795744e1425fb97958c385aa590d756667d2
SAINISAHAB-KK <kapilsainik83@gmail.com>
2021-05-29 10:11:22 +0530

    updated

--- python_pattern.py
+++ python_pattern.py
@@ -9,16 +9,22 @@
 # for i in range(n):print(("* "*(i+1)).center((n*2)-1))
 
 
-# n =int(input())
-# for i in range(1,n+1):
+# for i in range(1,int(input())+1):
 #     for  j in range(1,i+1):print(str(j)+ " ",end=" ")
 #     print()
 
 
-n = int(input())
-k=1
-for i in range(1,n+1):
-    for i in range(1,i):
-        print(k,end=" ")
-        k=k+1
-    print()
+# k=1
+# for i in range(1,(int(input())+1)):
+#     for i in range(1,i+1):
+#         print(k,end=" ");k=k+1
+#     print()
+
+
+# for _ in range (65,66+int(input())):print((chr(_)+" ")*(_-64))
+
+# n=65
+# for _ in range (1,int(input())+1):
+#     for j in range(1,_+1):
+#         print(chr(n),end=' ');n=n+1
+#     print()
